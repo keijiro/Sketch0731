f5f0093fd3223eb5e81db51e62ab6ebf6347564f
Keijiro Takahashi <keijiro@gmail.com>
2022-08-03 23:59:03 +0900

    ObjectPool

--- Assets/MeshBuilder.cs
+++ Assets/MeshBuilder.cs
@@ -4,7 +4,9 @@ using Random = Unity.Mathematics.Random;
 
 sealed class MeshBuilder : MonoBehaviour
 {
-    [SerializeField] int _rowCount = 10;
+    const int Capacity = 256;
+
+    [SerializeField, Range(0, Capacity)] int _rowCount = 10;
     [SerializeField] float _width = 0.1f;
     [SerializeField] float _length = 5;
     [SerializeField] int _resolution = 100;
@@ -13,46 +15,74 @@ sealed class MeshBuilder : MonoBehaviour
     [SerializeField] uint _seed = 10;
     [SerializeField] Material _material = null;
 
-    List<MeshConstructor> _meshes = new List<MeshConstructor>();
+    (GameObject go, MeshConstructor mesh) [] _pool
+      = new (GameObject, MeshConstructor)[Capacity];
+
+    void InitializeObjectPool()
+    {
+        var comps = new [] { typeof(MeshFilter), typeof(MeshRenderer) };
+
+        for (var i = 0; i < Capacity; i++)
+        {
+            var go = new GameObject($"Row{i}", comps);
+            go.hideFlags = HideFlags.DontSave;
+
+            go.transform.parent = transform;
+
+            go.GetComponent<MeshRenderer>().sharedMaterial
+              = new Material(_material);
+
+            _pool[i] = (go, null);
+        }
+    }
+
+    void OnDestroy()
+    {
+        for (var i = 0; i < Capacity; i++)
+        {
+            if (_pool[i].go != null) Destroy(_pool[i].go);
+            if (_pool[i].mesh != null) _pool[i].mesh.Dispose();
+            _pool[i] = (null, null);
+        }
+    }
 
     void Start()
     {
-        var comps = new [] { typeof(MeshFilter), typeof(MeshRenderer) };
+        InitializeObjectPool();
 
         var rand = new Random(_seed);
         rand.NextUInt4();
 
         for (var i = 0; i < _rowCount; i++)
         {
+            _pool[i].go.SetActive(true);
+            
+            if (_pool[i].mesh != null) _pool[i].mesh.Dispose();
+
             var cons = new MeshConstructor(_resolution)
               { Width = _width, Length = _length,
                 Frequency = rand.NextFloat(_frequency.x, _frequency.y),
                 Phase = rand.NextFloat(0, _bias) };
 
+            _pool[i].mesh = cons;
+
             cons.InitializeIndexBuffer();
             cons.InitializeVertexBuffer();
             cons.ConstructMesh();
 
-            var go = new GameObject($"Row{i}", comps);
-            go.GetComponent<MeshRenderer>().sharedMaterial = _material;
-            go.GetComponent<MeshFilter>().sharedMesh = cons.SharedMesh;
+            _pool[i].go.GetComponent<MeshFilter>().sharedMesh = cons.SharedMesh;
+            var material = _pool[i].go.GetComponent<MeshRenderer>().sharedMaterial;
 
-            go.transform.parent = transform;
-            go.transform.localPosition = new Vector3(i * _width, 0, 0);
-            go.transform.localRotation = Quaternion.identity;
-
-            _meshes.Add(cons);
+            _pool[i].go.transform.localPosition = new Vector3(i * _width, 0, 0);
+            _pool[i].go.transform.localRotation = Quaternion.identity;
 
             var color = Color.HSVToRGB(rand.NextFloat(), 1, 1);
             var intensity = rand.NextFloat() < 0.35f ? 200 : 0;
-            go.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", color);
-            go.GetComponent<MeshRenderer>().material.SetColor("_EmissiveColor", color * intensity);
+            material.SetColor("_BaseColor", color);
+            material.SetColor("_EmissiveColor", color * intensity);
         }
-    }
 
-    void OnDestroy()
-    {
-        foreach (var cons in _meshes) cons.Dispose();
-        _meshes.Clear();
+        for (var i = _rowCount; i < Capacity; i++)
+            _pool[i].go.SetActive(false);
     }
 }
