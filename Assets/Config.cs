b40289855ee581cbabb7b9f383690ec3fca3e714
Keijiro Takahashi <keijiro@gmail.com>
2022-08-06 01:19:22 +0900

    Refactoring to Node

--- Assets/Config.cs
+++ Assets/Config.cs
@@ -8,9 +8,11 @@ sealed class Config
     public readonly float Width = 0.1f;
     public readonly float Length = 5;
     public readonly Vector2 Frequency = new Vector2(10, 15);
-    public readonly float Bias = 0;
+    [Range(0, 1)] public readonly float Bias = 0.2f;
+    [Range(0, 1)] public readonly float BurstProbability = 0.2f;
+    [Range(0, 1)] public readonly float BurstIntensity = 200;
     public readonly int Resolution = 100;
-    public readonly uint Seed = 10;
+    public readonly uint Seed = 1234;
 }
 
 } // namespace Sketch0731
--- Assets/Node.cs
+++ Assets/Node.cs
@@ -5,22 +5,30 @@ namespace Sketch0731 {
 
 sealed class Node : MonoBehaviour
 {
-    static System.Type [] ComponentList
-      = new [] { typeof(MeshFilter), typeof(MeshRenderer), typeof(Node) };
-
-    Modeler _modeler;
+    #region Public factory method
 
     public static Node Create(Transform parent, Material material)
     {
+        // Create as a "don't save" object.
         var go = new GameObject("Node", ComponentList);
-        go.transform.parent = parent;
         go.hideFlags = HideFlags.DontSave;
-        go.GetComponent<MeshRenderer>().sharedMaterial = new Material(material);
-        return go.GetComponent<Node>();
+
+        // Parenting
+        go.transform.parent = parent;
+
+        // Reference to the instance
+        var node = go.GetComponent<Node>();
+
+        // Material copy
+        node._material = new Material(material);
+        go.GetComponent<MeshRenderer>().sharedMaterial = node._material;
+
+        return node;
     }
 
-    void OnDestroy()
-      => _modeler?.Dispose();
+    #endregion
+
+    #region Public methods
 
     public void Activate(Config config, int index)
     {
@@ -35,32 +43,72 @@ sealed class Node : MonoBehaviour
         _modeler = null;
     }
 
+    #endregion
+
+    #region Private objects
+
+    static System.Type [] ComponentList
+      = new [] { typeof(MeshFilter), typeof(MeshRenderer), typeof(Node) };
+
+    Modeler _modeler;
+    Material _material;
+
+    #endregion
+
+    #region MonoBehavior implementation
+
+    void OnDestroy()
+    {
+        if (_modeler != null)
+        {
+            _modeler?.Dispose();
+            _modeler = null;
+        }
+
+        if (_material != null)
+        {
+            Destroy(_material);
+            _material = null;
+        }
+    }
+
+    #endregion
+
+    #region Mesh operation
+
     void BuildMesh(Config config, int index)
     {
+        // Local random number generator
         var rand = new Random(config.Seed + (uint)index);
         rand.NextUInt4();
 
+        // Modeler setup
         _modeler?.Dispose();
         _modeler = new Modeler(config.Resolution)
-          { Width = config.Width, Length = config.Length,
+          { Width = config.Width,
+            Length = config.Length,
             Frequency = rand.NextFloat(config.Frequency.x, config.Frequency.y),
             Phase = rand.NextFloat(0, config.Bias) };
 
+        // Mesh construction
         _modeler.InitializeIndexBuffer();
         _modeler.InitializeVertexBuffer();
         _modeler.ConstructMesh();
-
         GetComponent<MeshFilter>().sharedMesh = _modeler.SharedMesh;
-        var material = GetComponent<MeshRenderer>().sharedMaterial;
 
+        // Coloring
+        var color = Color.HSVToRGB(rand.NextFloat(), 1, 1);
+        var burst = rand.NextFloat() < config.BurstProbability;
+        var intensity = burst ? config.BurstIntensity : 0;
+        _material.SetColor("_BaseColor", color);
+        _material.SetColor("_EmissiveColor", color * intensity);
+
+        // Node placement
         transform.localPosition = new Vector3(index * config.Width, 0, 0);
         transform.localRotation = Quaternion.identity;
-
-        var color = Color.HSVToRGB(rand.NextFloat(), 1, 1);
-        var intensity = rand.NextFloat() < 0.35f ? 200 : 0;
-        material.SetColor("_BaseColor", color);
-        material.SetColor("_EmissiveColor", color * intensity);
     }
+
+    #endregion
 }
 
 } // namespace Sketch0731
